082119a372751d200d74b9eed67e178df3e0199b
ruki <waruqi@gmail.com>
2014-08-29 17:54:55 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,111 @@
+ios objc 方法调用记录插件: itracer v1.2
+
+如果你想逆向 某些app的调用流程 或者 系统app的一些功能的 私有 framework class api 调用流程， 可以试试此工具
+
+只需要 配置需要挂接的 类名和app名， 就可以实时追踪 相关功能的 调用流程。 支持批量 hook n多个类名
+
+特性：
+* 批量跟踪ios下指定class对象的所有调用流程
+* 支持ios for armv6,armv7,arm64 以及mac for x86, x64
+* 自动探测参数类型，并且打印所有参数的详细信息
+
+更新内容：
+
+* 增加对arm64的支持，刚调通稳定性有待测试。
+   arm64进程注入没时间做了，暂时用了substrate的hookprocess， 所以大家需要先装下libsubstrate.dylib
+   armv7的版本是完全不依赖substrate的。
+
+* arm64的版本对参数的信息打印稍微做了些增强。
+
+注：此项目已不再维护，仅供参考。
+
+1. 配置需要挂接的class
+
+    ```
+    修改itrace.xml配置文件，增加需要hook的类名：
+    <?xml version="1.0" encoding="utf-8"?>
+    <itrace>
+      <class>
+        <SSDevice/>
+        <SSDownload/>
+        <SSDownloadManager/>
+        <SSDownloadQueue/>
+        <CPDistributedMessagingCenter/>
+        <CPDistributedNotificationCenter/>
+        <NSString args="0"/>
+      </class>
+    </itrace>
+    ```
+
+    注： 尽量不要去hook， 频繁调用的class， 比如 UIView NSString， 否则会很卡，操作就不方便了。
+    注： 如果挂接某个class， 中途打印参数信息挂了， 可以在对应的类名后面 加上 args="0" 属性， 来禁止打印参数信息， 这样会稳定点。 
+         如果要让所有类都不打印参数信息， 可以直接设置： <class args="0">
+
+
+2. 安装文件
+
+    将整个itracer目录下的所有文件用手机助手工具，上传到ios系统上的 /tmp 下面：
+    ```
+    /tmp/itracer
+    /tmp/itrace.dylib
+    /tmp/itrace.xml
+    ```
+
+3. 进行trace
+
+    进入itracer所在目录：
+    ```
+      cd /tmp
+      ```
+
+    修改执行权限：
+    ```
+      chmod 777 ./itracer
+    ```
+
+    运行程序: 
+
+    ```
+      ./itracer springboard (spingboard 为需要挂接的进程名， 支持简单的模糊匹配)
+    ```
+
+4. 查看 trace log， 注： log 的实际输出在： Xcode-Windows菜单-Organizer-Console 中：
+
+    ```
+    Jan 21 11:12:58 unknown SpringBoard[5706] <Warning>: [itrace]: [3edc9d98]: [SSDownloadQueue downloads]
+    Jan 21 11:12:58 unknown SpringBoard[5706] <Warning>: [itrace]: [3edc9d98]: [SSDownloadManager downloads]
+    Jan 21 11:12:58 unknown SpringBoard[5706] <Warning>: [itrace]: [3edc9d98]: [SSDownloadManager _copyDownloads]
+    Jan 21 11:12:58 unknown SpringBoard[5706] <Warning>: [itrace]: [3edc9d98]: [SSDownloadQueue _sendDownloadStatusChangedAtIndex:]: 0
+    Jan 21 11:12:58 unknown SpringBoard[5706] <Warning>: [itrace]: [3edc9d98]: [SSDownloadQueue _messageObserversWithFunction:context:]: 0x334c5d51: 0x2fe89de0
+    Jan 21 11:12:58 unknown SpringBoard[5706] <Warning>: [itrace]: [3edc9d98]: [SSDownloadQueue downloads]
+    Jan 21 11:12:58 unknown SpringBoard[5706] <Warning>: [itrace]: [3edc9d98]: [SSDownloadManager downloads]
+    Jan 21 11:12:58 unknown SpringBoard[5706] <Warning>: [itrace]: [3edc9d98]: [SSDownloadManager _copyDownloads]
+    Jan 21 11:12:58 unknown SpringBoard[5706] <Warning>: [itrace]: [3edc9d98]: [SSDownload cachedApplicationIdentifier]
+    Jan 21 11:12:58 unknown SpringBoard[5706] <Warning>: [itrace]: [3edc9d98]: [SSDownload status]
+    Jan 21 11:12:58 unknown SpringBoard[5706] <Warning>: [itrace]: [3edc9d98]: [SSDownload cachedApplicationIdentifier]
+    Jan 21 11:12:58 unknown SpringBoard[5706] <Warning>: [itrace]: [3edc9d98]: [CPDistributedNotificationCenter postNotificationName:userInfo:]: SBApplicationNotificationStateChanged: {
+          SBApplicationStateDisplayIDKey = "com.apple.AppStore";
+          SBApplicationStateKey = 2;
+          SBApplicationStateProcessIDKey = 5868;
+          SBMostElevatedStateForProcessID = 2;
+      }
+    Jan 21 11:12:58 unknown SpringBoard[5706] <Warning>: [itrace]: [3edc9d98]: [CPDistributedNotificationCenter postNotificationName:userInfo:toBundleIdentifier:]: SBApplicationNotificationStateChanged: {
+          SBApplicationStateDisplayIDKey = "com.apple.AppStore";
+          SBApplicationStateKey = 2;
+          SBApplicationStateProcessIDKey = 5868;
+          SBMostElevatedStateForProcessID = 2;
+      }: null
+    Jan 21 11:12:59 unknown SpringBoard[5706] <Warning>: [itrace]: [105d7000]: [SSDownloadManager _handleMessage:fromServerConnection:]: 0xe6920b0: 0xe007040
+    Jan 21 11:12:59 unknown SpringBoard[5706] <Warning>: [itrace]: [105d7000]: [SSDownloadManager _handleDownloadStatesChanged:]: 0xe6920b0
+    Jan 21 11:12:59 unknown SpringBoard[5706] <Warning>: [itrace]: [105d7000]: [SSDownloadManager _copyDownloads]
+    Jan 21 11:12:59 unknown SpringBoard[5706] <Warning>: [itrace]: [105d7000]: [SSDownload persistentIdentifier]
+    Jan 21 11:12:59 unknown SpringBoard[5706] <Warning>: [itrace]: [105d7000]: [SSDownload _addCachedPropertyValues:]: {
+          I = SSDownloadPhaseDownloading;
+      }
+    Jan 21 11:12:59 unknown SpringBoard[5706] <Warning>: [itrace]: [105d7000]: [SSDownload _applyPhase:toStatus:]: SSDownloadPhaseDownloading: <SSDownloadStatus: 0xe6b8e80>
+    Jan 21 11:12:59 unknown SpringBoard[5706] <Warning>: [itrace]: [105d7000]: [SSDownloadQueue downloadManager:downloadStatesDidChange:]: <SSDownloadManager: 0x41ea60>: (
+          "<SSDownload: 0xe6bd970>: -4085275246093726486"
+      )
+    ```
+
+
---
+++ inc/ios/armv7/tbox/database/sql.h
@@ -0,0 +1,333 @@
+/*!The Treasure Box Library
+ * 
+ * TBox is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ * 
+ * TBox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TBox; 
+ * If not, see <a href="http://www.gnu.org/licenses/"> http://www.gnu.org/licenses/</a>
+ * 
+ * Copyright (C) 2009 - 2015, ruki All rights reserved.
+ *
+ * @author		ruki
+ * @file		sql.h
+ * @defgroup 	database
+ */
+#ifndef TB_DATABASE_SQL_H
+#define TB_DATABASE_SQL_H
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * includes
+ */
+#include "prefix.h"
+#include "value.h"
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * types
+ */
+
+/// the sql database type enum
+typedef enum __tb_database_sql_type_e
+{
+	TB_DATABASE_SQL_TYPE_NONE 				= 0
+,	TB_DATABASE_SQL_TYPE_MYSQL 				= 1
+,	TB_DATABASE_SQL_TYPE_SQLITE3 			= 2
+
+}tb_database_sql_type_e;
+
+/// the database type
+typedef struct __tb_database_sql_t
+{
+	/// the url
+	tb_url_t 					url;
+
+	/// the type
+	tb_size_t 					type;
+
+	/// the state
+	tb_size_t 					state;
+
+	/// is opened?
+	tb_bool_t 					bopened;
+
+	/// open
+	tb_bool_t 					(*open)(struct __tb_database_sql_t* database);
+
+	/// clos
+	tb_void_t 					(*clos)(struct __tb_database_sql_t* database);
+
+	/// exit
+	tb_void_t 					(*exit)(struct __tb_database_sql_t* database);
+
+	/// done
+	tb_bool_t 					(*done)(struct __tb_database_sql_t* database, tb_char_t const* sql);
+
+	/// load result
+	tb_iterator_t* 				(*result_load)(struct __tb_database_sql_t* database, tb_bool_t try_all);
+
+	/// exit result
+	tb_void_t 					(*result_exit)(struct __tb_database_sql_t* database, tb_iterator_t* result);
+
+	/// stmt init
+	tb_handle_t 				(*stmt_init)(struct __tb_database_sql_t* database, tb_char_t const* sql);
+
+	/// stmt exit
+	tb_void_t 					(*stmt_exit)(struct __tb_database_sql_t* database, tb_handle_t stmt);
+
+	/// stmt done
+	tb_bool_t 					(*stmt_done)(struct __tb_database_sql_t* database, tb_handle_t stmt);
+
+	/// stmt bind
+	tb_bool_t 					(*stmt_bind)(struct __tb_database_sql_t* database, tb_handle_t stmt, tb_database_sql_value_t const* list, tb_size_t size);
+
+}tb_database_sql_t;
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * interfaces
+ */
+
+/*! init sql database
+ *
+ * @param url 		the database url
+ *                  "sql://localhost/?type=mysql&username=xxxx&password=xxxx"
+ *                  "sql://localhost:3306/?type=mysql&username=xxxx&password=xxxx&database=xxxx"
+ *                  "sql:///home/file.sqlitedb?type=sqlite3"
+ *                  "/home/file.sqlite3"
+ *                  "file:///home/file.sqlitedb"
+ *                  "C://home/file.sqlite3"
+ *
+ * @return 			tb_true or tb_false
+ */
+tb_handle_t 		tb_database_sql_init(tb_char_t const* url);
+
+/*! exit database
+ *
+ * @param database 	the database handle
+ */
+tb_void_t 			tb_database_sql_exit(tb_handle_t database);
+
+/*! the database type
+ *
+ * @param database 	the database handle
+ *
+ * @return 			the database type
+ */
+tb_size_t 			tb_database_sql_type(tb_handle_t database);
+
+/*! open database
+ *
+ * @code
+ * tb_handle_t database = tb_database_sql_init("sql://localhost/?type=mysql&username=xxxx&password=xxxx");
+ * if (database)
+ * {
+ *     // open it
+ *     if (tb_database_sql_open(database))
+ *     {
+ *          // done it
+ *          // ...
+ *
+ *          // close it
+ *          tb_database_sql_clos(database);
+ *     }
+ *     tb_database_sql_exit(database);
+ * }
+ * @endcode
+ *
+ * @param database 	the database handle
+ *
+ * @return 			tb_true or tb_false
+ */
+tb_bool_t 			tb_database_sql_open(tb_handle_t database);
+
+/*! clos database
+ *
+ * @param database 	the database handle
+ */
+tb_void_t 			tb_database_sql_clos(tb_handle_t database);
+
+/*! commit the current transaction
+ *
+ * @param database 	the database handle
+ *
+ * @return 			tb_true or tb_false
+ */
+tb_bool_t 			tb_database_sql_commit(tb_handle_t database);
+
+/*! rollback the current transaction
+ *
+ * @param database 	the database handle
+ *
+ * @return 			tb_true or tb_false
+ */
+tb_bool_t 			tb_database_sql_rollback(tb_handle_t database);
+
+/*! the database state
+ *
+ * TODO: ..
+ *
+ * @param database 	the database handle
+ *
+ * @return 			the database state
+ */
+tb_size_t 			tb_database_sql_state(tb_handle_t database);
+
+/*! done database
+ *
+ * @code
+ *
+ * // done sql
+ * if (!tb_database_sql_done(database, "select * from table"))
+ * {
+ *     // trace
+ *     tb_trace_e("done sql failed, error: %s", tb_state_cstr(tb_database_sql_state(database)));
+ *     return ;
+ * }
+ *
+ * // load result
+ * // ..
+ *
+ * @endcode
+ *
+ * @param database 	the database handle
+ * @param sql 		the sql command
+ *
+ * @return 			tb_true or tb_false
+ */
+tb_bool_t 			tb_database_sql_done(tb_handle_t database, tb_char_t const* sql);
+
+/*! load the database result
+ *
+ * @code
+ *
+ * // done sql
+ * // ..
+ *
+ * // load result
+ * tb_iterator_t* result = tb_database_sql_result_load(database, tb_true);
+ * if (result)
+ * {
+ *     // walk result
+ *     tb_for_all_if (tb_iterator_t*, row, result, row)
+ *     {
+ *          // walk values
+ *          tb_for_all_if (tb_database_sql_value_t*, value, row, value)
+ *          {
+ *               tb_trace_i("name: %s, data: %s, at: %lux%lu", tb_database_sql_value_name(value), tb_database_sql_value_text(value), row_itor, item_itor);
+ *          }
+ *     }
+ *
+ *     // exit result
+ *     tb_database_sql_result_exit(result);
+ * }
+ * 
+ * // load result
+ * tb_iterator_t* result = tb_database_sql_result_load(database, tb_false);
+ * if (result)
+ * {
+ *     // walk result
+ *     tb_for_all_if (tb_iterator_t*, row, result, row)
+ *     {
+ *          // field count
+ *          tb_trace_i("count: %lu", tb_iterator_size(row));
+ *
+ *          // id
+ *			tb_database_sql_value_t const* id = tb_iterator_item(row, 0);
+ *			if (id)
+ *          {
+ *               tb_trace_i("id: %d", tb_database_sql_value_int32(id));
+ *          }
+ *  
+ *          // name
+ *			tb_database_sql_value_t const* name = tb_iterator_item(row, 1);
+ *			if (name)
+ *          {
+ *               tb_trace_i("name: %s", tb_database_sql_value_text(name));
+ *          }
+ *     }
+ *
+ *     // exit result
+ *     tb_database_sql_result_exit(result);
+ * }
+ * @endcode
+ *
+ * @param database 	the database handle
+ * @param try_all 	try loading all result into memory
+ *
+ * @return 			the database result
+ */
+tb_iterator_t* 		tb_database_sql_result_load(tb_handle_t database, tb_bool_t try_all);
+
+/*! exit the database result
+ *
+ * @param database 	the database handle
+ * @param result 	the database result
+ */
+tb_void_t 			tb_database_sql_result_exit(tb_handle_t database, tb_iterator_t* result);
+
+/*! init the database stmt
+ *
+ * @param database 	the database handle
+ * @param sql 		the sql command
+ *
+ * @return 			the stmt handle
+ */
+tb_handle_t 		tb_database_sql_stmt_init(tb_handle_t database, tb_char_t const* sql);
+
+/*! exit the database stmt
+ *
+ * @param database 	the database handle
+ * @param stmt 		the stmt handle
+ */
+tb_void_t 			tb_database_sql_stmt_exit(tb_handle_t database, tb_handle_t stmt);
+
+/*! done the database stmt
+ *
+ * @code
+ * tb_handle_t stmt = tb_database_sql_stmt_init(database, "select * from table where id=?");
+ * if (stmt)
+ * {
+ *      // bind arguments
+ *      tb_database_sql_value_t list[1];
+ *      tb_database_sql_value_set_int32(&list[0], 12345);
+ *      if (tb_database_sql_stmt_bind(database, stmt, list, tb_arrayn(list)))
+ *      {
+ *            // done stmt
+ *            if (tb_database_sql_stmt_done(database, stmt))
+ *            {
+ *                  // load result
+ *                  // ...
+ *            }
+ *      }
+ *
+ *      // exit stmt
+ *      tb_database_sql_stmt_exit(database, stmt);
+ * }
+ * @endcode
+ *
+ * @param database 	the database handle
+ * @param stmt 		the stmt handle
+ *
+ * @return 			tb_true or tb_false
+ */
+tb_bool_t 			tb_database_sql_stmt_done(tb_handle_t database, tb_handle_t stmt);
+
+/*! bind the database stmt argument
+ *
+ * @param database 	the database handle
+ * @param stmt 		the stmt handle
+ * @param list 		the argument value list
+ * @param size 		the argument value count
+ *
+ * @return 			tb_true or tb_false
+ */
+tb_bool_t 			tb_database_sql_stmt_bind(tb_handle_t database, tb_handle_t stmt, tb_database_sql_value_t const* list, tb_size_t size);
+
+
+#endif
